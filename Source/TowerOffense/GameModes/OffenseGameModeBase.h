98b978e75625bb0042781c43f853dd8f384e6a17
Patryk Urbański <patryk.urbanski@thingtrunk.com>
2021-12-13 21:55:29 +0100

    Fixed copyright comments

--- Source/TowerOffense/GameModes/OffenseGameModeBase.h
+++ Source/TowerOffense/GameModes/OffenseGameModeBase.h
@@ -1,5 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Created by Patryk Urbañski 2021
 #pragma once
 
 #include "CoreMinimal.h"
--- Source/TowerOffense/Pawns/PawnTank.h
+++ Source/TowerOffense/Pawns/PawnTank.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Created by Patryk Urbañski 2021
 
 #pragma once
 
